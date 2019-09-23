c37adc69e64e01548cae38a31883908ebcac0b2d
Illia <illiabasharov@gmail.com>
2019-09-23 21:08:58 +0300

    without normal registration

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -64,9 +64,11 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            'name' => $data['name'],
+            'login' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'role' => 1,
+            'is_publish' => 1
         ]);
     }
 }
