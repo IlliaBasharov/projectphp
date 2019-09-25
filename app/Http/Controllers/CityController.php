4b1df3345893fbbe400ea503a4ab38207f241502
Illia <illiabasharov@gmail.com>
2019-09-25 23:59:58 +0300

    get routes

--- app/Http/Controllers/CityController.php
+++ app/Http/Controllers/CityController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class CityController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('city');
+    }
+
+    public function about()
+    {
+        return view('about');
+    }
 }
---
+++ resources/views/teacher.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.app')
+@section('content')
+    <h1>Заполнить инфой</h1>
+@endsection
